038a94f95e3cf3c149957b932c59eea3b4d18a9a
Leonardo Laranjeira <leonardo02lfc@gmail.com>
2025-12-02 20:27:22 -0300

    Novas alterações importantes...
    Aplicando primeiros conceitos de interatividade e acessibilidade.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-const barra = document.getElementById('bars_menu')
-const tema = document.getElementById('botao_tema')
-
 // Menu escondido para celular
 
 
@@ -12,3 +9,41 @@ const tema = document.getElementById('botao_tema')
 
  - Mudar o transform: translateX() do .slider-track quando as setas ou bolinhas forem clicadas.
  */
+
+document.getElementById('perfil').addEventListener('click', function() {
+    // 1. Usa o método scrollTo para rolar a janela (window)
+    window.scrollTo({
+        // Define o topo (0) como a posição de destino
+        top: 0, 
+        // Define o comportamento da rolagem como suave (smooth)
+        behavior: 'smooth' 
+    });
+});
+
+ /**
+   * Mobile nav toggle
+   */
+  const mobileNav = document.querySelector('#bars_menu');
+
+  function mobileNavToogle() {
+    const isExpanded = document.querySelector('body').classList.toggle('mobile-menu');
+    mobileNav.classList.toggle('listar');
+    mobileNav.classList.toggle('fechar');
+
+    mobileNav.setAttribute('aria-expanded', isExpanded);
+  }
+  if (mobileNav) {
+    mobileNav.addEventListener('click', mobileNavToogle);
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#menu a').forEach(menu => {
+    menu.addEventListener('click', () => {
+      if (document.querySelector('#bars_menu')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
